14c8b066896744289dd994341c87c2c5bfdd2d3c
Aeril <xiebingqingaeril@gmail.com>
2019-02-06 16:28:02 -0800

    Update api.py
    fix query syntax error; wrap aws endpoint with local route;

--- api.py
+++ api.py
@@ -20,69 +20,37 @@ app.config["DEBUG"] = True
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '****'
 
+#app configuration 
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'frenzy2018datafeedsearchengine'
+
 def Wrap(feed, url, subid):
 	new_link = ' '
-	if feed == 'rakuten':
+	if feed == 'Rakuten':
 		if str(url).startswith(''):
 			new_link = 'http://click.linksynergy.com/link?' + url + '&u1=' + subid
 		if str(url).startswith('http'):
 			new_link = url + '&u1=' + subid
 
-	if feed == 'cj':
+	elif feed == 'cj':
 		if str(url).startswith('url'):
-			new_link = 'http://www.dpbolvw.net/click-8810497-11192081?' + 'sid='+ subid + '&' + url 
+			new_link = 'http://www.dpbolvw.net/click-8810497-11192081?sid='+ subid + '&' + url 
 		if str(url).startswith('http://'):
-			new_link = url[0:url.find('?')]+'sid='+ subid + '&' + url[url.find('?'):]
+			new_link = url[0:url.find('?')]+'?sid='+ subid + '&' + url[url.find('?')+1:]
 
-	if feed == 'Pepperjam':
+	elif feed == 'Pepperjam':
 		if str(url).startswith('url'):
-			new_link = '	http://www.pjtra.com/t/Qz9JSkJGP0NKRURGRT9JSkJG?' + 'sid='+ subid + '&' + url 
+			new_link = '	http://www.pjtra.com/t/Qz9JSkJGP0NKRURGRT9JSkJG?sid='+ subid + '&' + url 
 		if str(url).startswith('http://'):
-			new_link = url[0: url.find('?')]+'sid='+ subid + '&' + url[url.find('?'):]
+			new_link = url[0: url.find('?')]+'?sid='+ subid + '&' + url[url.find('?')+1:]
 
-	if feed == 'ShareASale':
+	elif feed == 'ShareASale':
 		new_link = url + 'afftrack=' + subid
 
 	return new_link
-
-
-def searchproducts(brand, category, subid):
-
-	client = boto3.client('cloudsearchdomain',
-	        endpoint_url = '***')
-	###  change request content
-	request = "(and brand:'brandname' category:'categoryname')"
-	request = request.replace("brandname",brand).replace("categoryname",category)
-	#####  change search paremeters
-	response = client.search(query=request,
-		    queryParser='structured',
-		    returnFields="brand,category,product_name,brand,product_url,image_url,description,color,material,merch_id,merchant_name,price,size,source",
-		    size=10000) #maxiamum size is 10,000
-
-	for i in response['hits']['hit']:
-		Wrap( i['fields']['source'][0] , i['fields']['product_url'][0], subid)
-
-	result = response['hits']['hit']
-
-	if response['hits']['hit']:
-		return jsonify(result), 200
-
-	return "product not found", 404
-
-
-def process_query(json_query):
-	json_dict = json.loads(json_query)
-	number = len(json_dict['hits']['hit'])
-	new_file = {"search_count":number, "hits":{}}
-	count = 1
-	for item in json_dict['hits']['hit']:
-		brand = item['brand']
-		category = item['category']
-		subID = item['sub_ID']
-		new_file["hits"]=searchproducts(brand,category,subID)
-		count +=1
-	with open('result.json','w') as f:
-		json.dump(new_file,f)
 
 
 
@@ -90,21 +58,113 @@ def process_query(json_query):
 def home():
     return "<h1>This is Frenzy Search: </h1><p>This site is a prototype API for search products according to key word.</p>"
 
+@app.route('/products/<brand>/<category>/<subid>', methods=['GET'])
+def searchproductsbybrand(brand, category, subid):
+    
+    client = boto3.client('cloudsearchdomain',
+	endpoint_url = '  ')
+	###  change request content
+    query = "(and brand:'brandname' category:'categoryname')"
+    query = query.replace("brandname",brand).replace("categoryname", category)
+	#####  change search paremeters
+    response = client.search(query=query,
+		    queryParser='structured',
+		    returnFields="brand,category,title,product_url,img_url,description,color,material,merch_id,merchant_name,price,size,source",
+		    size=10000) #maxiamum size is 10,000
+  
+    for i in response['hits']['hit']:
+        i['fields']['product_url'][0]= Wrap( i['fields']['source'][0] , i['fields']['product_url'][0], subid)
+    
+    result = response['hits']['hit']
 
-@app.route('/upload', methods = ['POST'])
-def api_upload():
-    if request.headers['Content-Type'] == 'application/json':
-        json_content = json.dumps(request.json)
-        process_query(json_content)
-        file = send_file('result.json', as_attachment=True)
-        os.system('rm result.json')
-        return file
-    else:
-        return "415 Unsupported Media Type ;(\n"
+    if response['hits']['hit']:
+        return jsonify(result), 200
+    
+    return "product not found", 404
 
 
+@app.route('/products/<color>/<category>/<subid>', methods=['GET'])
+def searchproductsbycolor(color, category, subid):
+    
+    client = boto3.client('cloudsearchdomain',
+	endpoint_url = '   ')
+	###  change request content
+    query = "(and color:'color' category:'categoryname')"
+    query = query.replace("color",color).replace("categoryname", category)
+	#####  change search paremeters
+    response = client.search(query=query,
+		    queryParser='structured',
+		    returnFields="brand,category,title,product_url,img_url,description,color,material,merch_id,merchant_name,price,size,source",
+		    size=10000) #maxiamum size is 10,000
+  
+    for i in response['hits']['hit']:
+        i['fields']['product_url'][0]= Wrap( i['fields']['source'][0] , i['fields']['product_url'][0], subid)
+    
+    result = response['hits']['hit']
+
+    if response['hits']['hit']:
+        return jsonify(result), 200
+    
+    return "product not found", 404
+
+
+@app.route('/products/<description>/<subid>', methods=['GET'])
+def searchproductsbydescription(description, subid):
+    
+    client = boto3.client('cloudsearchdomain',
+	endpoint_url = '  ')
+    response = client.search(query="(and description: '" + str(description)+"')",
+		    queryParser='structured',
+		    returnFields="brand,category,title,product_url,img_url,description,color,material,merch_id,merchant_name,price,size,source",
+		    size=10000) #maxiamum size is 10,000
+  
+    for i in response['hits']['hit']:
+        i['fields']['product_url'][0]= Wrap( i['fields']['source'][0] , i['fields']['product_url'][0], subid)
+    
+    result = response['hits']['hit']
+
+    if response['hits']['hit']:
+        return jsonify(result), 200
+    
+    return "product not found", 404
+
+#<string:category><string:brand><string:color><string:description><int:subid>
+@app.route('/products/mysearch', methods=['GET'])
+def searchindetail():
+    category = request.args.get('category')
+    brand = request.args.get('brand')
+    color = request.args.get('color')
+    description = request.args.get('desc')
+    subid = request.args.get('subid')
+    print(category,brand,color,description,subid)
+    
+    client = boto3.client('cloudsearchdomain',
+	endpoint_url = ' ')
+	###  change request content
+    query = "(and color: '"+color+ "' category: '"+ category+ "' brand: '"+brand + "' description: '"+description+"')"
+    
+    print(query)
+    #####  change search paremeters
+    response = client.search(query=query,
+		    queryParser='structured',
+		    returnFields="brand,category,title,product_url,img_url,description,color,material,merch_id,merchant_name,price,size,source",
+		    size=10000) #maxiamum size is 10,000
+    
+    for i in response['hits']['hit']:
+        i['fields']['product_url'][0]= Wrap( i['fields']['source'][0] , i['fields']['product_url'][0], subid)
+    
+    result = response['hits']['hit']
+
+    if response['hits']['hit']:
+        return jsonify(result), 200
+    
+    return "product not found", 404
+    
 
 if __name__ == '__main__':
      app.run(port='****')
+
+
+
 
 
